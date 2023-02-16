64ea065d5adbf571ad255c59126a6d81005da786
Adrian Pop <adrian.pop@liu.se>
2023-02-16 14:56:23 +0100

    update the batch files

--- batch/buildNightly64bit.bat
+++ batch/buildNightly64bit.bat
@@ -7,8 +7,11 @@ REM last change: 2015-05-08
 
 if not exist "c:\OMDev\" (
   echo Checkout c:\OMDev\
-  cd c:\
-  svn co https://openmodelica.org/svn/OpenModelicaExternal/trunk/tools/windows/OMDev OMDev
+  git clone https://openmodelica.org/git/OMDev.git OMDev
+  cd C:\OMDev
+  git checkout master
+  call SETUP_OMDEV.bat
+  call SETUP_OMDEV_Qt5.bat
   set OMDEV=c:\OMDev
 )
 
@@ -28,28 +31,28 @@ if not exist "c:\dev\OpenModelica_releases" (
  md c:\dev\OpenModelica_releases
 )
   
-if not exist "c:\dev\OpenModelica64bit\" (
-  echo Checkout c:\dev\OpenModelica64bit
+if not exist "c:\dev\OM64bit\" (
+  echo Checkout c:\dev\OM64bit
   cd c:\dev\
-  git clone --recursive https://github.com/OpenModelica/OpenModelica.git OpenModelica64bit
-  cd OpenModelica64bit
+  git clone --recursive https://github.com/OpenModelica/OpenModelica.git OM64bit
+  cd OM64bit
 )
 
-if not exist "c:\dev\OpenModelica64bit\OpenModelicaSetup\" (
-  echo Checkout c:\dev\OpenModelica64bit\OpenModelicaSetup
-  cd c:\dev\OpenModelica64bit\
-  svn co https://openmodelica.org/svn/OpenModelica/installers/windows/OpenModelicaSetup OpenModelicaSetup
+if not exist "c:\dev\OM64bit\OMSetup\" (
+  echo Checkout c:\dev\OM64bit\OMSetup
+  cd c:\dev\OM64bit\
+  git clone https://github.com/OpenModelica/OpenModelicaSetup OMSetup
 )
 
 REM update the build script first!
-cd c:\dev\OpenModelica64bit\OpenModelicaSetup
-svn up . --accept theirs-full
+cd c:\dev\OM64bit\OMSetup
+git pull
 
 REM update OMDev
 cd C:\OMDev
-svn up . --accept theirs-full
+git pull
 
 REM run the Msys script to build the release
 cd c:\dev\OpenModelica_releases\
 set MSYSTEM=MINGW64
-%OMDEV%\tools\msys\usr\bin\sh --login -i -c "time /c/dev/OpenModelica64bit/OpenModelicaSetup/BuildWindowsRelease.sh adrpo -j3 64bit %1%"
+%OMDEV%\tools\msys\usr\bin\sh --login -i -c "time /c/dev/OM64bit/OMSetup/BuildWindowsRelease.sh adrpo -j3 64bit %1%"
