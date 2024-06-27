af9e92e5a001f5cccb0ded4c0811ae323d26f90c
Adeel Asghar <adeel.asghar@liu.se>
2024-06-27 22:24:46 +0200

    Filter dirs and files separately (#66)

--- GenerateFilesList.py
+++ GenerateFilesList.py
@@ -36,13 +36,14 @@ import os
 import argparse
 import re
 
-def list_files(base_dir, exclude_files_dirs, f, recursive):
+def list_files(base_dir, exclude_dirs, exclude_files, f, recursive):
   # Compile regular expressions for exclusions
-  exclude_files_dirs_regex = [re.compile(pattern) for pattern in exclude_files_dirs]
+  exclude_dirs_regex = [re.compile(pattern) for pattern in exclude_dirs]
+  exclude_files_regex = [re.compile(pattern) for pattern in exclude_files]
 
   for root, dirs, files in os.walk(base_dir):
     # Exclude specified directories
-    dirs[:] = [d for d in dirs if not any(regex.match(d) for regex in exclude_files_dirs_regex)]
+    dirs[:] = [d for d in dirs if not any(regex.match(d) for regex in exclude_dirs_regex)]
     reset_output_path = False
     if root != base_dir:
       nested_dir = root.replace(base_dir, '')
@@ -52,7 +53,7 @@ def list_files(base_dir, exclude_files_dirs, f, recursive):
       f.write('${SetOutPath} "$R0' + nested_dir + '"\n')
 
     for file in files:
-      if not any(regex.match(file) for regex in exclude_files_dirs_regex):
+      if not any(regex.match(file) for regex in exclude_files_regex):
         file_path = os.path.join(root, file)
         file_path = file_path.replace('/','\\')
         f.write('${File} "' + file_path + '" "' + file + '"\n')
@@ -75,7 +76,7 @@ if __name__ == "__main__":
   # Create bin directory and copy files in it
   f.write('${SetOutPath} "\\\\?\\$INSTDIR\\bin"' + '\n')
   base_directory = "..\\build\\bin"
-  list_files(base_directory, [], f, False)
+  list_files(base_directory, [], [], f, False)
   f.write('${File} "..\OSMC-License.txt" "OSMC-License.txt"' + '\n')
   # Copy the openssl binaries
   if args.PLATFORMVERSION == "32":
@@ -90,28 +91,28 @@ if __name__ == "__main__":
   OMDEV = os.environ['OMDEV']
   OMDEV = OMDEV.replace('/', '\\')
   base_directory = OMDEV + "\\tools\msys\\" + args.MSYSRUNTIME + args.PLATFORMVERSION + "\share\qt5\plugins"
-  files_dirs_to_exclude = [r"\*.svn"]
-  list_files(base_directory, files_dirs_to_exclude, f, True)
+  files_to_exclude = [r".*\.git"]
+  list_files(base_directory, [], files_to_exclude, f, True)
   # Create the bin\osgPlugins-3.6.5 directory
   f.write('${SetOutPath} "\\\\?\\$INSTDIR\\bin\osgPlugins-3.6.5"' + '\n')
   base_directory = OMDEV + "\\tools\msys\\" + args.MSYSRUNTIME + args.PLATFORMVERSION + "\\bin\osgPlugins-3.6.5"
-  files_dirs_to_exclude = [r"\*.svn"]
-  list_files(base_directory, files_dirs_to_exclude, f, True)
+  files_to_exclude = [r".*\.git"]
+  list_files(base_directory, [], files_to_exclude, f, True)
   # Create bin\ffi directory and copy files in it
   f.write('${SetOutPath} "\\\\?\\$INSTDIR\\bin\\ffi"' + '\n')
   base_directory = "..\\build\\bin\\ffi"
-  list_files(base_directory, [], f, False)
+  list_files(base_directory, [], [], f, False)
   # Create bin\omc-semla directory and copy files in it
   f.write('!if /FILEEXISTS "..\\build\\bin\omc-semla\*.*"' + '\n')
   f.write('${SetOutPath} "\\\\?\\$INSTDIR\\bin\omc-semla"' + '\n')
   base_directory = "..\\build\\bin\omc-semla"
-  list_files(base_directory, [], f, True)
+  list_files(base_directory, [], [], f, True)
   f.write('!endif' + '\n')
   # Create icons directory and copy files in it
   f.write('${SetOutPath} "\\\\?\\$INSTDIR\icons"' + '\n')
   base_directory = "icons"
-  files_dirs_to_exclude = [r"\*.svn"]
-  list_files(base_directory, files_dirs_to_exclude, f, True)
+  files_to_exclude = [r".*\.git"]
+  list_files(base_directory, [], files_to_exclude, f, True)
   f.write('${File} "..\OMEdit\OMEditLIB\Resources\icons\omedit.ico" "omedit.ico"' + '\n')
   f.write('${File} "..\OMOptim\OMOptim\GUI\Resources\omoptim.ico" "omoptim.ico"' + '\n')
   f.write('${File} "..\OMPlot\OMPlot\OMPlotGUI\Resources\icons\omplot.ico" "omplot.ico"' + '\n')
@@ -121,19 +122,19 @@ if __name__ == "__main__":
   f.write('${AddItem} "\\\\?\\$INSTDIR\include"' + '\n')
   f.write('${SetOutPath} "\\\\?\\$INSTDIR\include\omc"' + '\n')
   base_directory = "..\\build\include\omc"
-  files_dirs_to_exclude = [r"\*.svn"]
-  list_files(base_directory, files_dirs_to_exclude, f, True)
+  files_to_exclude = [r".*\.git"]
+  list_files(base_directory, [], files_to_exclude, f, True)
   # Create lib\omc directory and copy files in it
   f.write('${AddItem} "\\\\?\\$INSTDIR\lib"' + '\n')
   f.write('${SetOutPath} "\\\\?\\$INSTDIR\lib\omc"' + '\n')
   base_directory = "..\\build\lib\omc"
-  files_dirs_to_exclude = [r"\*.svn", r"\*.git"]
-  list_files(base_directory, files_dirs_to_exclude, f, True)
+  files_to_exclude = [r".*\.git"]
+  list_files(base_directory, [], files_to_exclude, f, True)
   # Create lib\python directory and copy files in it
   f.write('${SetOutPath} "\\\\?\\$INSTDIR\lib\python"' + '\n')
   base_directory = "..\\build\lib\python"
-  files_dirs_to_exclude = [r"\*.svn", r"\*.git"]
-  list_files(base_directory, files_dirs_to_exclude, f, True)
+  files_to_exclude = [r".*\.git"]
+  list_files(base_directory, [], files_to_exclude, f, True)
   # Create tools directory and copy files in it
   f.write('${SetOutPath} "\\\\?\\$INSTDIR\\tools"' + '\n')
   # copy the setup file / readme
@@ -142,37 +143,40 @@ if __name__ == "__main__":
   # Create msys directory and copy files in it
   f.write('${SetOutPath} "\\\\?\\$INSTDIR\\tools\msys"' + '\n')
   base_directory = OMDEV + "\\tools\msys"
-  files_dirs_to_exclude = [r"group", r"passwd", r"pacman.log", r"tmp\.*\.*", r".*\.pyc", r"libQt5.*\.*", r"libQt6.*\.*"
-                           , r"moc.exe", r"qt.*\.qch", r"Qt5.*\.*", r"qt5.*\.*", r"Qt6.*\.*", r"qt6.*\.*", r"libwx.*\.*", r"libgtk.*\.*", r"\\bqtcreator\\b"
-                           , r"rcc.exe", r"testcon.exe", r"libsicu.*\.*", r"libicu.*\.*", r"wx.*\.dll", r"libosg.*\.*", r"\\bAdwaita\\b"
-                           , r"\\bOpenSceneGraph\\b", r"\\bgtk-doc\\b" , r"\\bpoppler\\b", r"\\bman\\b", r"libdbus.*", r"tcl.*\.*", r"avcodec.*\.*"
-                           , r"windeployqt.exe", r"\\bpython3.5\\b", r"mingw_osg.*\.*" , r"\\bActiveQt\\b", r"\\bQt3DCore\\b", r"\\bQt3DInput\\b"
-                           , r"\\bQt3DLogic\\b", r"\\bQt3DQuick\\b", r"\\bQt3DQuickInput\\b" , r"\\bQt3DQuickRender\\b", r"\\bQt3DRender\\b"
-                           , r"\\bQtBluetooth\\b", r"\\bQtCLucene\\b", r"\\bQtConcurrent\\b", r"\\bQtCore\\b"
-                           , r"\\bQtDBus\\b", r"\\bQtDesigner\\b", r"\\bQtDesignerComponents\\b", r"\\bQtGui\\b", r"\\bQtHelp\\b", r"\\bQtLabsControls\\b"
-                           , r"\\bQtLabsTemplates\\b", r"\\bQtLocation\\b", r"\\bQtMultimedia\\b", r"\\bQtMultimediaQuick_p\\b", r"\\bQtMultimediaWidgets\\b"
-                           , r"\\bQtNetwork\\b", r"\\bQtNfc\\b", r"\\bQtOpenGL\\b", r"\\bQtOpenGLExtensions\\b", r"\\bQtPlatformHeaders\\b", r"\\bQtPlatformSupport\\b"
-                           , r"\\bQtPositioning\\b", r"\\bQtPrintSupport\\b", r"\\bQtQml\\b", r"\\bQtQmlDevTools\\b", r"\\bQtQuick\\b", r"\\bQtQuickParticles\\b"
-                           , r"\\bQtQuickTest\\b", r"\\bQtQuickWidgets\\b", r"\\bQtScript\\b", r"\\bQtScriptTools\\b", r"\\bQtSensors\\b", r"\\bQtSerialBus\\b"
-                           , r"\\bQtSerialPort\\b", r"\\bQtSql\\b", r"\\bQtSvg\\b", r"\\bQtTest\\b", r"\\bQtUiPlugin\\b", r"\\bQtUiTools\\b", r"\\bQtWebChannel\\b"
-                           , r"\\bQtWebKit\\b", r"\\bQtWebKitWidgets\\b", r"\\bQtWebSockets\\b", r"\\bQtWidgets\\b", r"\\bQtWinExtras\\b", r"\\bQtXml\\b"
-                           , r"\\bQtXmlPatterns\\b" , r"\\bosg\\b", r"\\bosgAnimation\\b", r"\\bosgDB\\b", r"\\bosgFX\\b", r"\\bosgGA\\b"
-                           , r"\\bosgManipulator\\b", r"\\bosgParticle\\b" , r"\\bosgPresentation\\b"
-                           , r"\\bosgQt\\b", r"\\bosgShadow\\b", r"\\bosgSim\\b", r"\\bosgTerrain\\b", r"\\bosgText\\b", r"\\bosgUI\\b", r"\\bosgUtil\\b", r"\\bosgViewer\\b"
-                           , r"\\bosgVolume\\b", r"\\bosgWidget\\b", r"clang-cl.exe", r"clang-check.exe", r"llvm-lto2.exe", r"\\bdoc\\b", r".gitignore"]
+  dirs_to_exclude = [r"tmp", r"qtcreator", r"Adwaita", r"OpenSceneGraph", r"gtk-doc" , r"poppler", r"man"
+                     , r"python3.5", r"ActiveQt", r"Qt3DCore", r"Qt3DInput", r"Qt3DLogic", r"Qt3DQuick"
+                     , r"Qt3DQuickInput" , r"Qt3DQuickRender", r"Qt3DRender", r"QtBluetooth", r"QtCLucene", r"QtConcurrent"
+                     , r"QtCore", r"QtDBus", r"QtDesigner", r"QtDesignerComponents", r"QtGui", r"QtHelp", r"QtLabsControls"
+                     , r"QtLabsTemplates", r"QtLocation", r"QtMultimedia", r"QtMultimediaQuick_p", r"QtMultimediaWidgets"
+                     , r"QtNetwork", r"QtNfc", r"QtOpenGL", r"QtOpenGLExtensions", r"QtPlatformHeaders", r"QtPlatformSupport"
+                     , r"QtPositioning", r"QtPrintSupport", r"QtQml", r"QtQmlDevTools", r"QtQuick", r"QtQuickParticles"
+                     , r"QtQuickTest", r"QtQuickWidgets", r"QtScript", r"QtScriptTools", r"QtSensors", r"QtSerialBus"
+                     , r"QtSerialPort", r"QtSql", r"QtSvg", r"QtTest", r"QtUiPlugin", r"QtUiTools", r"QtWebChannel"
+                     , r"QtWebKit", r"QtWebKitWidgets", r"QtWebSockets", r"QtWidgets", r"QtWinExtras", r"QtXml"
+                     , r"QtXmlPatterns" , r"osg", r"osgAnimation", r"osgDB", r"osgFX", r"osgGA", r"osgManipulator"
+                     , r"osgParticle" , r"osgPresentation", r"osgQt", r"osgShadow", r"osgSim", r"osgTerrain"
+                     , r"osgText", r"osgUI", r"osgUtil", r"osgViewer", r"osgVolume", r"osgWidget", r"doc"]
+
+  files_to_exclude = [r"group", r"passwd", r"pacman.log", r".*\.pyc", r"libQt5.*\.*", r"libQt6.*\.*"
+                      , r"moc.exe", r"qt.*\.qch", r"Qt5.*\.*", r"qt5.*\.*", r"Qt6.*\.*", r"qt6.*\.*", r"libwx.*\.*", r"libgtk.*\.*"
+                      , r"rcc.exe", r"testcon.exe", r"libsicu.*\.*", r"libicu.*\.*", r"wx.*\.dll", r"libosg.*\.*", r"libdbus.*", r"tcl.*\.*", r"avcodec.*\.*"
+                      , r"windeployqt.exe", r"mingw_osg.*\.*", r"clang-cl.exe", r"clang-check.exe", r"llvm-lto2.exe", r".gitignore"]
+
   if args.PLATFORMVERSION == "32":
-    files_dirs_to_exclude = files_dirs_to_exclude + [r"\\bmingw64\\b", r"\\bucrt64\\b", r"\\bclang64\\b"]
+    dirs_to_exclude = dirs_to_exclude + [r"mingw64", r"ucrt64", r"clang64"]
   else: # 64 bit
     if args.MSYSRUNTIME == "ucrt":
-      files_dirs_to_exclude = files_dirs_to_exclude + [r"\\bmingw32\\b", r"\\bmingw64\\b", r"\\bclang64\\b", r"\\bclang32\\b"]
+      dirs_to_exclude = dirs_to_exclude + [r"mingw32", r"mingw64", r"clang64", r"clang32"]
     else: # mingw64
-      files_dirs_to_exclude = files_dirs_to_exclude + [r"\\bmingw32\\b", r"\\bucrt64\\b", r"\\bclang64\\b", r"\\bclang32\\b"]
-  list_files(base_directory, files_dirs_to_exclude, f, True)
+      dirs_to_exclude = dirs_to_exclude + [r"mingw32", r"ucrt64", r"clang64", r"clang32"]
+  list_files(base_directory, dirs_to_exclude, files_to_exclude, f, True)
+  # Create tmp folder for msys
+  f.write('${SetOutPath} "\\\\?\\$INSTDIR\\tools\msys\\tmp"' + '\n')
   # Create share directory and copy files in it
   f.write('${SetOutPath} "\\\\?\\$INSTDIR\\share"' + '\n')
   base_directory = "..\\build\share"
-  files_dirs_to_exclude = [r"\*.svn", r"\*.git"]
-  list_files(base_directory, files_dirs_to_exclude, f, True)
+  files_to_exclude = [r".*\.git"]
+  list_files(base_directory, [], files_to_exclude, f, True)
   # Copy the OpenModelica web page & users guide url shortcut
   f.write('${AddItem} "\\\\?\\$INSTDIR\share\doc"' + '\n')
   f.write('${SetOutPath} "\\\\?\\$INSTDIR\\share\doc\omc"' + '\n')
@@ -181,7 +185,7 @@ if __name__ == "__main__":
   # Copy OMSens directory
   f.write('${SetOutPath} "\\\\?\\$INSTDIR\\share\OMSens"' + '\n')
   base_directory = "..\\build\share\OMSens"
-  files_dirs_to_exclude = [r"\*.git"]
-  list_files(base_directory, files_dirs_to_exclude, f, True)
+  files_to_exclude = [r".*\.git"]
+  list_files(base_directory, [], files_to_exclude, f, True)
   f.close
   print("Successfully generated file FilesList.nsh")
